27175bae37b2b73ed438f04be74d1455cee1ebd2
imantha-das <imantha@yahoo.com>
2024-09-22 10:56:53 +0800

    aggGP code

---
+++ aggGP.py
@@ -0,0 +1,157 @@
+# ==============================================================================
+# Script to Run Aggregated Gaussian Process on Influenza Cases at differnt 
+# resolution administrative boundaries.
+# ==============================================================================
+# ---------------------------------- Imports --------------------------------- #
+
+import os
+import numpy as np 
+import pandas as pd
+import geopandas as gpd
+
+import time 
+
+import jax
+import jax.numpy as jnp 
+from jax import random
+import numpyro
+import numpyro.distributions as dist
+from numpyro.infer import NUTS, MCMC
+
+import dill
+# ---------------------------- GP Kernel Function ---------------------------- #
+def dist_euclid(x, z):
+    """
+    Computes Eucledian Distance Between Regions. This function is used by
+    exp_sq_kernel function (kernel function for gaussian processes)
+    """
+    x = jnp.array(x) # (ngrid_pts, lat/lon) <- i.e (2618,2)
+    z = jnp.array(z) # (ngrid_pts, lat/lon) <- i.e (2618,2)
+    if len(x.shape)==1:
+        x = x.reshape(x.shape[0], 1) #(2618,) -> (2618,1)
+    if len(z.shape)==1:
+        z = x.reshape(x.shape[0], 1) #(2618,) -> (2618,1)
+    n_x, m = x.shape # 2618 , 2
+    n_z, m_z = z.shape # 2618 , 2
+    assert m == m_z
+    delta = jnp.zeros((n_x,n_z)) #(ngrid_pts,ngrid_pts) <- i.e (2618,2618)
+    for d in jnp.arange(m):
+        x_d = x[:,d] #(ngrid_pts-lat/lon,) <- (2618,) 
+        z_d = z[:,d] #(ngrid_pts-lat/lon,) <- (2618,) 
+        delta += (x_d[:,jnp.newaxis] - z_d)**2 # (2618,2618)
+
+    return jnp.sqrt(delta) #(2618,2618)
+
+
+def exp_sq_kernel(x, z, var, length, noise, jitter=1.0e-4):
+    dist = dist_euclid(x, z) #(2618, 2618)
+    deltaXsq = jnp.power(dist/ length, 2.0) 
+    k = var * jnp.exp(-0.5 * deltaXsq)
+    k += (noise + jitter) * jnp.eye(x.shape[0])
+    return k # (ngrid_pts, ngrid_pts) <- (2618,2618)
+
+# --------------------------- Aggregation functions -------------------------- #
+def M_g(M, g):
+    '''
+    - $M$ is a matrix with binary entries $m_{ij},$ showing whether point $j$ is in polygon $i$
+    - $g$ is a vector of GP draws over grid
+    - $maltmul(M, g)$ gives a vector of sums over each polygon
+    '''
+    M = jnp.array(M)
+    g = jnp.array(g).T
+    return(jnp.matmul(M, g))
+
+# ------------------------ Aggregated prevelance model ----------------------- #
+def case_est_model_gp_aggr(args, y = None):
+    """Aggregated Gaussian Process model"""
+    #n_cases = args["n_low_obs"] # (8,)
+    n_specimens = args["n_low_specs"]
+    #? we dont need high resolution data as we are estimating it
+    #n_hi_obs = args["n_high_obs"] #(48,)
+    gp_kernel = args["gp_kernel"]
+    noise = args["noise"]
+    jitter = args["jitter"]
+    M_lo = args["M1"]
+    #? we dont need high resolution data as we are estimating it
+    #M2 = args["pol_pt_hi"]
+
+    # random effects - aggregated GP 
+    # Hyperparamas for kernel covariance
+    length = numpyro.sample("kernel_length", dist.InverseGamma(3,3))
+    var = numpyro.sample("kernel_var", dist.HalfNormal(0.05))
+    # GP Kernel
+    k = gp_kernel(x,x,var, length, noise, jitter)
+    # GP Draw
+    f = numpyro.sample(
+        "f", 
+        dist.MultivariateNormal(
+            loc = jnp.zeros(x.shape[0]), 
+            covariance_matrix= k)
+        )
+    
+    # aggregated f into gp_aggr according to indexing of (point in polygon)
+    gp_aggr = numpyro.deterministic("gp_aggr_lo", M_g(M_lo, f))
+    
+    # fixed effects
+    b0 = numpyro.sample("b0", dist.Normal(0,1))
+    # Linear predictor
+    lp = b0 + gp_aggr 
+    theta = numpyro.deterministic("theta", jax.nn.sigmoid(lp))
+
+    #todo : Check with Swapnil if "n_specimens" is the right argument to pass to "total_count"
+    numpyro.sample("cases", dist.BinomialLogits(total_count= n_specimens, logits = lp), obs = y)
+
+
+if __name__ == "__main__":
+    # --------------------------------- Load Data -------------------------------- #
+    data_dir = "data/processed"
+    
+    # Lat/Lon Values of artificial grid
+    x = np.load(os.path.join(data_dir,"lat_lon_x.npy")) #(2618,2)
+    # Low regional data
+    pol_pt_lo = np.load(os.path.join(data_dir,"low","pol_pt_lo.npy")) #(9,2618)
+    pt_which_pol_lo = np.load(os.path.join(data_dir,"low","pt_which_pol_lo.npy")) #(2618,)
+    # High regional data 
+    pol_pt_hi = np.load(os.path.join(data_dir,"high","pol_pt_hi.npy")) #(49, 2618,)
+    pt_which_pol_hi = np.load(os.path.join(data_dir,"high","pt_which_pol_hi.npy")) #(2618,)
+    # Dataframes
+    df_lo = gpd.read_file(os.path.join(data_dir, "low", "us_census_divisions","us_census_divisions.shp"))
+    df_hi = gpd.read_file(os.path.join(data_dir, "high", "us_state_divisions","us_state_divisions.shp"))
+    
+    # ---------------------------- Variables for Model ---------------------------- #
+    args = {
+        #"n_low_obs" : df_lo.tot_cases, #observarions <- This is passed as y
+        "n_low_specs" : jnp.array(df_lo.tot_specs),  
+        "x" : jnp.array(x),
+        "gp_kernel" : exp_sq_kernel,
+        "jitter" : 1e-4,
+        "noise" : 1e-4,
+        "M1" : pol_pt_lo,
+        #todo : You can probably remove M2 as this is not needed
+        "M2" : pol_pt_hi,
+        "combine" : False # Get combined results for 
+    }
+
+    # ---------------------------- Aggregated GP model --------------------------- #
+    run_key, predict_key = random.split(random.PRNGKey(3))
+    n_warm = 200
+    n_samples = 1000
+    mcmc = MCMC(
+        NUTS(case_est_model_gp_aggr), 
+        num_warmup=n_warm,
+        num_samples = n_samples
+    )
+
+    # --------------------------------- Run MCMC --------------------------------- #
+    start = time.time()
+    mcmc.run(run_key, args, y = np.array(df_lo.tot_cases))
+    end = time.time()
+    print(f"Time taken for aggGP : {end - start}'s")
+
+    # -------------------------------- Save Model -------------------------------- #
+    with open(f"model_weights/aggGP_samples{n_samples}_tt{end-start}s", 'wb') as file:
+        dill.dump(mcmc, file)
+
+    print("\nMCMC elapsed time:", round(end), "s")
+    print("\nMCMC elapsed time:", round(end/60), "min")
+    print("\nMCMC elapsed time:", round(end/(60*60)), "h")
---
+++ aggVAE.py
@@ -0,0 +1,244 @@
+# ==============================================================================
+# Script to run Aggregated Variational AutoEncoder (aggVAE) on Influenza Cases 
+# at differnt resolution administrative boundaries.
+# ==============================================================================
+# ---------------------------------- imports --------------------------------- #
+
+import os
+import math
+import numpy as np 
+
+import time
+
+import jax 
+import jax.numpy as jnp 
+from jax import random, lax, jit, ops
+from jax.example_libraries import stax
+
+import numpyro 
+from numpyro.infer import SVI, MCMC, NUTS, init_to_median, Predictive, RenyiELBO 
+import numpyro.distributions as dist  
+
+import geopandas as gpd
+import plotly.express as px
+
+from termcolor import colored
+
+from aggGP import dist_euclid, exp_sq_kernel, M_g
+
+# ------------------- Func for Prior Predictive Simulation ------------------- #
+def gp_aggr(args):
+    x = args["x"]
+    gp_kernel = args["gp_kernel"]
+    noise = args["noise"]
+    
+    M_low = args["M_low"] #M_old in original code
+
+    # Random effect - aggregated GP 
+    length = numpyro.sample("kernel_length", dist.InverseGamma(3,3))
+    var = numpyro.sample("kernel_var",dist.HalfNormal(0.05))
+    k = gp_kernel(x,x,var, length, noise)
+    f = numpyro.sample(
+        "f", 
+        dist.MultivariateNormal(loc = jnp.zeros(x.shape[0]), covariance_matrix = k)
+        )
+
+    #aggregate f into gp_aggr according to indexing of (point in polygon)
+    gp_aggr_lo = numpyro.deterministic("gp_aggr_lo", M_g(M_low, f))
+    return gp_aggr_lo
+    
+# -------------------------- Variational Autoencoder ------------------------- #
+def vae_encoder(hidden_dim, z_dim):
+    return stax.serial(
+        stax.Dense(hidden_dim, W_init = stax.randn()),
+        stax.Elu,
+        stax.FanOut(2),
+        stax.parallel(
+            # mean 
+            stax.Dense(z_dim, W_init = stax.randn()),
+            #std 
+            stax.serial(stax.Dense(z_dim, W_init = stax.randn()), stax.Exp)
+        )
+    )
+
+def vae_decoder(hidden_dim, out_dim):
+    return stax.serial(
+        stax.Dense(hidden_dim, W_init = stax.randn()),
+        stax.Elu,
+        stax.Dense(out_dim, W_init = stax.randn())
+    )
+
+
+def vae_model(batch, hidden_dim, z_dim):
+    batch = jnp.reshape(batch, (batch.shape[0], -1)) # still gonna be (5,116)
+    batch_dim, out_dim = jnp.shape(batch) # 5 , 116 
+
+    # vae-decoder in numpyro module
+    decode = numpyro.module(
+        name = "decoder", 
+        nn = vae_decoder(hidden_dim = hidden_dim, out_dim = out_dim),
+        input_shape = (batch_dim, z_dim)    
+    )
+
+    # Sample a univariate normal
+    #! ISSUE HERE : lax.sub cannot broadcast shapes (5,40) & (40,) here 
+    #! SO HAD TO CHANGE dist.Normal(jnp.zeros((z_dim,)), jnp.ones((z_dim,))) TO THIS dist.Normal(jnp.zeros((5,z_dim)), jnp.ones((5,z_dim)))
+    z = numpyro.sample("z", dist.Normal(jnp.zeros((batch_dim,z_dim)), jnp.ones((batch_dim,z_dim)))) #(z_dim,) <- i.e (40,)
+    # Forward pass from decoder
+    gen_loc = decode(z) #(num_regions,) <- (116,)
+    obs = numpyro.sample("obs", dist.Normal(gen_loc, args["vae_var"]), obs = batch) #(num_samples, num_regions) <- (5,116)
+    return obs
+
+
+def vae_guide(batch, hidden_dim, z_dim):
+    batch = jnp.reshape(batch, (batch.shape[0], -1)) #(num_samples, num_regions) <- (5,116)
+    batch_dim, input_dim = jnp.shape(batch)# num_samples , num_regions <- 5 , 116 
+
+    # vae-encoder in numpyro module
+    encode = numpyro.module(
+        name = "encoder", 
+        nn = vae_encoder(hidden_dim=hidden_dim,z_dim = z_dim),
+        input_shape = (batch_dim, input_dim)    
+    ) 
+
+    # Samapling mu, sigma - Pretty much the forward pass
+    z_loc, z_std = encode(batch) #mu : (num_samples, z_dim), sigma2 : (num_samples, z_dim) <- (5,40),(5,40)
+
+    z = numpyro.sample("z", dist.Normal(z_loc, z_std)) #(num_sample, z_dim) <- (5,40)
+    return z
+
+@jit 
+def epoch_train(rng_key, svi_state, num_train):
+    def body_fn(i, val):
+        # Random keys
+        rng_key_i = random.fold_in(rng_key, i)
+        rng_key_i, rng_key_ls, rng_key_var, rng_key_noise = random.split(rng_key_i, 4)
+        loss_sum, svi_state = val 
+        # Gp Draw 
+        batch = agg_gp_predictive(rng_key, args)["gp_aggr_lo"] #(num_samples, num_regions) <- (5,116)
+        # Forward pass, Takes "state" & "args" where args are your "model inputs" 
+        svi_state, loss = svi.update(svi_state, batch)
+        loss_sum += loss / args["batch_size"]
+        return loss_sum, svi_state 
+    
+    return lax.fori_loop(lower =0, upper = num_train, body_fun = body_fn, init_val = (0.0, svi_state))
+
+@jit
+def eval_test(rng_key, svi_state, num_test):
+    def body_fn(i, loss_sum):
+        rng_key_i = random.fold_in(rng_key, i)
+        rng_key_i, rng_key_ls, rng_key_var, rng_key_noise = random.split(rng_key_i, 4)
+        batch = agg_gp_predictive(rng_key_i, args)["gp_aggr_lo"] #(num_samples, num_regions) <- GP Draw
+        loss = svi.evaluate(svi_state, batch)
+        loss_sum += loss 
+        return loss 
+    loss = lax.fori_loop(0, num_test, body_fn, 0.0)
+    loss = loss / num_test
+
+    return loss
+      
+# -------------------- Function to plot a gaussian process ------------------- #
+def plot_process(gp_draws):
+    p = px.line()
+    for i in range(len(gp_draws)):
+        p.add_scatter(x = np.arange(gp_draws.shape[1]), y = gp_draws[i, :])
+
+    p.update_traces(line_color = "black")
+    p.update_layout(
+        template = "plotly_white", 
+        xaxis_title = "region", yaxis_title = "num cases",
+        showlegend = False
+    )
+    p.show()
+
+
+if __name__ == "__main__":
+    # ------------------------------ Load variables ------------------------------ #
+    # root directory
+    var_dir = "data/processed"
+    # Lat/Lon Values of artificial grid
+    x = np.load(os.path.join(var_dir,"lat_lon_x.npy")) #(2618,2)
+    # Low regional data
+    pol_pt_lo = np.load(os.path.join(var_dir,"low","pol_pt_lo.npy")) #(9,2618)
+    pt_which_pol_lo = np.load(os.path.join(var_dir,"low","pt_which_pol_lo.npy")) #(2618,)
+    # High regional data 
+    pol_pt_hi = np.load(os.path.join(var_dir,"high","pol_pt_hi.npy")) #(49, 2618,)
+    pt_which_pol_hi = np.load(os.path.join(var_dir,"high","pt_which_pol_hi.npy")) #(2618,)
+    # Dataframes
+    df_lo = gpd.read_file(os.path.join(var_dir, "low", "us_census_divisions","us_census_divisions.shp"))
+    df_hi = gpd.read_file(os.path.join(var_dir, "high", "us_state_divisions","us_state_divisions.shp"))
+
+    # --------------------------------- Arguments -------------------------------- #
+    args = {
+        "x": x,
+        "gp_kernel": exp_sq_kernel,
+        "noise": 1e-4,
+        "M_low": pol_pt_lo,
+
+        # VAE training
+        "rng_key": random.PRNGKey(5),
+        "num_epochs": 20, 
+        #"learning_rate": 1.0e-3, 
+        "learning_rate": 0.0005, 
+        "batch_size": 100, 
+        "hidden_dim": 50, 
+        "z_dim": 40, 
+        "num_train": 100,
+        "num_test":100,
+        "vae_var": 1,
+
+        # Consider One Region or Both
+        "consider_m1" : False
+    }
+
+    # ------------------------ Prior Predictive Simulation ----------------------- #
+    rng_key, rng_key_ = random.split(random.PRNGKey(4))
+    agg_gp_predictive = Predictive(gp_aggr,num_samples = 5)
+    # Returns (n_Samples, num_regions_in_m1)
+    agg_gp_draws = agg_gp_predictive(rng_key_, args)["gp_aggr_lo"] #(num_sample, num_regions)
+    
+   
+    # Plotting 
+    #plot_process(agg_gp_draws)
+    
+    #-------------------------- Initiate Training Loop -------------------------- #
+    adam = numpyro.optim.Adam(step_size = args["learning_rate"])
+    svi = SVI(
+        vae_model, 
+        vae_guide, 
+        adam, 
+        RenyiELBO(), 
+        hidden_dim = args["hidden_dim"], 
+        z_dim = args["z_dim"]
+    )
+
+    rng_key, rng_key_samp, rng_key_init = random.split(args["rng_key"],3)
+    init_batch = agg_gp_predictive(rng_key_, args)["gp_aggr_lo"] #(num_samples, num_regions) i.e (5, 116)
+    svi_state = svi.init(rng_key_init, init_batch)
+    
+    test_loss_list = []
+    for i in range(args["num_epochs"]):
+        rng_key, rng_key_train, rng_key_test, rng_key_infer = random.split(rng_key, 4)
+        t_start = time.time()
+
+        num_train = 1000
+        # Where forward/backward pass gets called for train
+        _ , svi_state = epoch_train(rng_key_train, svi_state, num_train)
+
+        num_test = 1000 
+        # Where forward/backward pass gets called for test
+        test_loss = eval_test(rng_key_test, svi_state, num_test)
+        test_loss_list += [test_loss]
+
+        print("Epoch : {}, loss : {} ({:.2f} s.)".format(i, test_loss, time.time() - t_start))
+
+        if math.isnan(test_loss):
+            break 
+    
+
+
+
+    
+     
+    
+
